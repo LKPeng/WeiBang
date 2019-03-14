a2c48a0b6f95eccab990959361ba2d1a40a887bc
吴凯耀 <kyit00g@gmail.com>
2019-03-14 11:09:18 +0800

    tab-title

--- WeiBang/WeiBang/Common/BaseClass/Controller/TabBar/WBTabBarController.m
+++ WeiBang/WeiBang/Common/BaseClass/Controller/TabBar/WBTabBarController.m
@@ -37,14 +37,13 @@
 - (void)setUpAllChildViewController {
     
     HomeViewController *home = [[HomeViewController alloc] init];
-    [self setUpOneChildViewController:home image:[UIImage imageNamed:@"nav_home"] selectedImage:[UIImage imageNamed:@"nav_home"] title:@"Home"];
+    [self setUpOneChildViewController:home image:[UIImage imageNamed:@"nav_home"] selectedImage:[UIImage imageNamed:@"nav_home"] title:@"首页"];
     
     MsgViewController *msg = [[MsgViewController alloc] init];
     [self setUpOneChildViewController:msg image:[UIImage imageNamed:@"nav_find"] selectedImage:[UIImage imageNamed:@"nav_find"] title:@"投资区"];
     
-    
     MyAccountViewController *me = [[MyAccountViewController alloc] init];
-    [self setUpOneChildViewController:me image:[UIImage imageNamed:@"nav_me"] selectedImage:[UIImage imageNamed:@"nav_me"] title:@"Me"];
+    [self setUpOneChildViewController:me image:[UIImage imageNamed:@"nav_me"] selectedImage:[UIImage imageNamed:@"nav_me"] title:@"我的账户"];
 }
 - (void)setUpOneChildViewController:(UIViewController *)vc image:(UIImage *)image selectedImage:(UIImage *)selectedImage title:(NSString *)title {
     vc.title = title;
